1f24a9bd3caa8d23a93d5379e24b041119457748
Ali Aliyev <alyvofficial@gmail.com>
2024-10-05 13:56:14 +0400

    Add Home component and configure Tailwind CSS

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {},
+    screens: {
+      sm: "100px",
+      md: "768px",
+      lg: "1024px",
+      xl: "1280px",
+      "2xl": "1536px",
+      tablet: "768px",
+      laptop: "1024px",
+      desktop: "1280px",
+    },
+  },
+  plugins: [],
+};
